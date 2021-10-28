5d9c1b95babb6fad3c791a6ed089a1d3bb6734c8
Daniel Bruno <daniel@hexcode.com.mx>
2021-10-28 13:05:37 -0700

    quotes added to text file,now print 2 random quotes,removed extra line after printing quote

--- get-quote.py
+++ get-quote.py
@@ -6,7 +6,8 @@ def start():
   f.close()
   last=len(quotes)-1
   rnd=random.randint(0, last)
-  print(quotes[rnd])
-
+  print(quotes[rnd],end='')
+  rnd=random.randint(0, last)
+  print(quotes[rnd],end='')
 if __name__== "__main__":
   start()
